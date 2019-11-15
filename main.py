c3489fc58bdc7246fc764d2198ddf50fae9b96ff
omer <omerbh1@gmail.com>
2019-11-15 13:56:22 +0200

    git ignore

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import tensorflow as tf
-import keras
-from keras import Model
-from keras.layers import Conv2D, Flatten, Dense, MaxPooling2D
 
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, MaxPooling2D
+import numpy as np
 
 class CNNModel(Model):
     def __init__(self):
@@ -26,6 +27,75 @@ class CNNModel(Model):
         return x
 
 
+def get_data(db, normalize = True):
+
+    if db == 'mnist':
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    else:
+        return None
+
+    if normalize:
+        x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    return  (x_train, y_train), (x_test, y_test)
+
+
+
 if __name__ == '__main__':
     model = CNNModel()
-    pass
+    (x_train, y_train), (x_test, y_test) = get_data('mnist')
+    x_train = x_train[..., tf.newaxis]
+    x_test = x_test[..., tf.newaxis]
+    train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
+    test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+    test_loss = tf.keras.metrics.Mean(name='test_loss')
+    test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+
+    optimizer = tf.keras.optimizers.Adam()
+
+    @tf.function
+    def train_step(images, labels):
+        with tf.GradientTape() as tape:
+            predictions = model(images)
+            loss = loss_object(labels, predictions)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+        train_loss(loss)
+        train_accuracy(labels, predictions)
+
+
+    @tf.function
+    def test_step(images, labels):
+        predictions = model(images)
+        t_loss = loss_object(labels, predictions)
+
+        test_loss(t_loss)
+        test_accuracy(labels, predictions)
+
+    EPOCHS = 5
+
+    for epoch in range(EPOCHS):
+        for images, labels in train_ds:
+            train_step(images, labels)
+
+        for test_images, test_labels in test_ds:
+            test_step(test_images, test_labels)
+
+        template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+        print(template.format(epoch + 1,
+                              train_loss.result(),
+                              train_accuracy.result() * 100,
+                              test_loss.result(),
+                              test_accuracy.result() * 100))
+
+        # Reset the metrics for the next epoch
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+        test_loss.reset_states()
+        test_accuracy.reset_states()
