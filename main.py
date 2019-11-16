abda57a7e8148039b25a574b7c27f7f664df4ac9
ori_yt <ori_yt@cs.huji.ac.il>
2019-11-16 20:02:04 +0200

    all answers.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from tensorflow.keras.layers import Conv2D, Flatten, Dense, MaxPooling2D
 
 class CNNModel(Model):
 
-
     def __init__(self):
         super(CNNModel, self).__init__()
         self.conv1 = Conv2D(32, (5, 5), activation='relu')
@@ -33,7 +32,6 @@ class CNNModel(Model):
 
 class LinearModel(Model):
 
-
     def __init__(self):
         super(LinearModel, self).__init__()
         self.max_pool_1 = MaxPooling2D((2, 2))
@@ -50,26 +48,36 @@ class LinearModel(Model):
         x = self.d2(x)
         return x
 
+
 class SmallCNN(Model):
 
     def __init__(self):
         super(SmallCNN, self).__init__()
-        self.conv1 = Conv2D(32, (5, 5), activation='relu')
-        self.max_pool_1 = MaxPooling2D((2, 2))
-        self.conv2 = Conv2D(64, (5, 5), activation='relu')
-        self.max_pool_2 = MaxPooling2D((2, 2))
+        self.conv1 = Conv2D(32, (3, 3), activation='relu')
         self.flatten = Flatten()
-        self.d1 = Dense(1024, activation='relu')
         self.d2 = Dense(10, activation='softmax')
 
+    def call(self, x, **kwargs):
+        x = self.conv1(x)
+        x = self.flatten(x)
+        x = self.d2(x)
+        return x
+
+class ReducedCNNModel(Model):
+
+    def __init__(self):
+        super(ReducedCNNModel, self).__init__()
+        self.conv1 = Conv2D(32, (3, 3), activation='relu')
+        self.max_pool_1 = MaxPooling2D((2, 2))
+        self.max_pool_2 = MaxPooling2D((2, 2))
+        self.flatten = Flatten()
+        self.d2 = Dense(10, activation='softmax')
 
     def call(self, x, **kwargs):
         x = self.conv1(x)
         x = self.max_pool_1(x)
-        x = self.conv2(x)
         x = self.max_pool_2(x)
         x = self.flatten(x)
-        x = self.d1(x)
         x = self.d2(x)
         return x
 
@@ -88,7 +96,9 @@ def get_data(db, normalize=True):
 
 def main():
     # model = CNNModel()
-    model = LinearModel()
+    # model = LinearModel()
+    # model = SmallCNN()
+    model = ReducedCNNModel()
     test_ds, train_ds = create_data_sets()
     loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
     train_step, train_loss, train_accuracy = get_train_step(model, loss_object)
@@ -105,7 +115,7 @@ def main():
     report_every = 500
     train_counter = 0
 
-    for epoch in range(report_every, total_number_of_iteration+report_every, report_every): # TODO it's not epoch!
+    for epoch in range(report_every, total_number_of_iteration + report_every, report_every):  # TODO it's not epoch!
         for images, labels in train_ds:
             train_step(images, labels)
             train_counter += 1
