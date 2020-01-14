2269b18d4e538472e31c5fcf2f14b0be6884c4f6
ori_yt <ori_yt@cs.huji.ac.il>
2020-01-14 23:52:12 +0200

    move to colab

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import models as exModels
 from tqdm.auto import tqdm
 
+
 def get_data(normalize=True):
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
@@ -13,7 +14,6 @@ def get_data(normalize=True):
     x_train, x_test = x_train[..., tf.newaxis], x_test[..., tf.newaxis]
 
     return (x_train, y_train), (x_test, y_test)
-
 
 
 def get_data_as_tensorslice():
@@ -37,9 +37,11 @@ def getSummaryWriters(modelName):
     test_summary_writer = tf.summary.create_file_writer(test_log_dir)
     return train_summary_writer, test_summary_writer
 
+
 def get_train_step(generator: tf.keras.Model, loss_object):
     optimizer = tf.keras.optimizers.Adam()
     train_loss = tf.keras.metrics.Mean(name='train_loss')
+    # TODO train_accuracy seems to be wrong maybe not , I'm not sure...
     train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
 
     @tf.function
@@ -55,7 +57,8 @@ def get_train_step(generator: tf.keras.Model, loss_object):
 
     return train_step, train_loss, train_accuracy
 
-def trainEncoder(generator, train_ds,epochs = 40):
+
+def trainEncoder(generator, train_ds, epochs=40, save_img_every=100):
     loss_object = tf.keras.losses.MeanSquaredError()
     train_step, train_loss, train_accuracy = get_train_step(generator, loss_object)
     train_summary_writer, test_summary_writer = getSummaryWriters(generator.name)
@@ -64,6 +67,14 @@ def trainEncoder(generator, train_ds,epochs = 40):
         for images, labels in train_ds:
             train_step(images, labels)
             train_counter += 1
+            if train_counter % save_img_every == 0:
+                with train_summary_writer.as_default():
+                    image_idx = 0
+                    tf.summary.image(
+                        "generator_img",
+                        generator(images[image_idx]),
+                        train_counter,
+                        description=f"generator_img for label {labels[image_idx]}")
 
         with train_summary_writer.as_default():
             tf.summary.scalar("loss", train_loss.result(), step=train_counter)
@@ -73,20 +84,21 @@ def trainEncoder(generator, train_ds,epochs = 40):
         train_loss.reset_states()
         train_accuracy.reset_states()
 
-
     generator.save_weights("./weights/v1")
     train_summary_writer.close()
     test_summary_writer.close()
 
 
-def Q1(epochs = 40):
+def Q1(epochs=40):
     generator = exModels.CNNGenerator()
     test_ds, train_ds = get_data_as_tensorslice()
     exModels.printable_model(generator).summary()
-    trainEncoder(generator, train_ds,epochs)
+    trainEncoder(generator, train_ds, epochs)
+
 
 def main():
     Q1()
+
 
 if __name__ == '__main__':
     main()
