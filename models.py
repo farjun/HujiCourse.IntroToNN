9fd263469e4fdb64940c6d402c30d63ca633f019
omer <omerbh1@gmail.com>
2020-01-14 19:42:34 +0200

    ex3 - its running, not sure yet what comes out. need to run in colab

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from tensorflow.keras import Model, Input
-from tensorflow.keras.layers import Conv2D, Conv2DTranspose, Dense, Reshape
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, Dense, Flatten
 
 
 def printable_model(model: Model, input_shape=(28, 28, 1)):
@@ -10,25 +10,23 @@ class CNNGenerator(Model):
 
     def __init__(self):
         super(CNNGenerator, self).__init__()
+        self.flatten = Flatten()
         self.conv1 = Conv2D(32, (3, 3), strides=(2,2), activation='relu')
         self.conv2 = Conv2D(64, (3, 3), strides=(2,2), activation='relu')
         self.d1 = Dense(512, activation='relu')
-        self.d2 = Dense(10, activation='sigmoid')
-        self.d3 = Dense(10, activation='relu')
-        self.d4 = Dense(512, activation='relu')
+        self.d2 = Dense(10, activation='relu')
+        self.d4 = Dense(512, activation='sigmoid')
         self.conv3 = Conv2DTranspose(64, (3, 3), strides=(2,2), activation='relu')
-        self.conv4 = Conv2DTranspose(32, (3, 3), strides=(2,2), activation='relu')
-        self.reshaper = Reshape((32,16))
+        self.conv4 = Conv2DTranspose(32, (4, 4), strides=(2,2), activation='relu')
 
 
     def call(self, x, **kwargs):
         x = self.conv1(x)
         x = self.conv2(x)
+        # x = self.flatten(x)
         x = self.d1(x)
         x = self.d2(x)
-        x = self.d3(x)
         x = self.d4(x)
-        x = self.reshaper(x)
         x = self.conv3(x)
         x = self.conv4(x)
 
