bf3523ff667db6ef0443f904ee9bbb2ca03ab70d
omer <omerbh1@gmail.com>
2020-01-15 23:33:47 +0200

    Q3 first version

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import tensorflow as tf
 from datetime import datetime
 import models as exModels
 from tqdm.auto import tqdm
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
 # WEIGHTS_PATH = "./weights/AE/v1"
+BATCH_SIZE = 32
 WEIGHTS_PATH = "./weights/v1"
 DenoisingAE_WEIGHTS_PATH = "./weights/DenoisingAE/v1"
+GanGenerator_WEIGHTS_PATH = "./weights/GanGenerator/v1"
+GanDiscriminator_WEIGHTS_PATH = "./weights/GanDiscriminator/v1"
 
 
 def get_data(normalize=True):
@@ -24,8 +28,8 @@ def get_data(normalize=True):
 
 def get_data_as_tensorslice(normalize=True):
     (x_train, y_train), (x_test, y_test) = get_data(normalize)
-    train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
-    test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+    train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(BATCH_SIZE)
+    test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(BATCH_SIZE)
     return test_ds, train_ds
 
 
@@ -61,7 +65,6 @@ def get_train_step(generator: tf.keras.Model, loss_object):
         train_accuracy(images, predictions)
 
     return train_step, train_loss, train_accuracy
-
 
 def train_AE(generator, train_ds, epochs=40, save_img_every=100, weights_path=WEIGHTS_PATH):
     loss_object = tf.keras.losses.MeanSquaredError()
@@ -102,6 +105,81 @@ def train_AE(generator, train_ds, epochs=40, save_img_every=100, weights_path=WE
     train_summary_writer.close()
     test_summary_writer.close()
 
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+def get_gan_train_step(generator: tf.keras.Model, discriminator: tf.keras.Model, generator_loss_object, discriminator_loss_object ):
+    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+    generator_train_loss = tf.keras.metrics.Mean(name='train_loss')
+    discriminator_train_loss = tf.keras.metrics.Mean(name='train_loss')
+    generator_train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+    discriminator_train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+    @tf.function
+    def train_step(images, labels):
+        noise = tf.random.normal((BATCH_SIZE, 1))
+        with tf.GradientTape() as gen_tape,tf.GradientTape() as disc_tape:
+            fake_im = generator(noise)
+
+            real_im_output = discriminator(images)
+            fake_im_output = discriminator(fake_im)
+
+            generator_loss = generator_loss_object(fake_im_output)
+            discriminator_loss = discriminator_loss_object(real_im_output, fake_im_output)
+
+        generator_gradients = gen_tape.gradient(generator_loss, generator.trainable_variables)
+        discriminator_gradients = disc_tape.gradient(discriminator_loss, discriminator.trainable_variables)
+
+        generator_optimizer.apply_gradients(zip(generator_gradients, generator.trainable_variables))
+        discriminator_optimizer.apply_gradients(zip(discriminator_gradients, discriminator.trainable_variables))
+
+        generator_train_loss(generator_loss)
+        discriminator_train_loss(discriminator_loss)
+
+        generator_train_accuracy(real_im_output, tf.zeros_like(real_im_output))
+        discriminator_train_accuracy(real_im_output, tf.ones_like(real_im_output))
+
+    return train_step, generator_train_loss, discriminator_train_loss, generator_train_accuracy, discriminator_train_accuracy
+
+def train_GAN(generator, discriminator, train_ds, epochs=40, save_img_every=100, gen_weights_path=WEIGHTS_PATH, disc_weights_path=WEIGHTS_PATH):
+    train_step, gen_train_loss, disc_train_loss, gen_train_accuracy, disc_train_accuracy = get_gan_train_step(generator, discriminator, generator_loss, discriminator_loss)
+    gen_train_summary_writer, t1 = getSummaryWriters(generator.name)
+    disc_train_summary_writer, t2 = getSummaryWriters(discriminator.name)
+    train_counter = 0
+    for epoch in tqdm(range(epochs)):
+        for images, labels in train_ds:
+            train_step(images, labels)
+            train_counter += 1
+            if train_counter % save_img_every == 0:
+                with gen_train_summary_writer.as_default():
+                    tf.summary.scalar("gen - loss", gen_train_loss.result(), step=train_counter)
+                    tf.summary.scalar("gen - accuracy", gen_train_accuracy.result() * 100, step=train_counter)
+
+                with disc_train_summary_writer.as_default():
+                    tf.summary.scalar("disc - loss", disc_train_loss.result(), step=train_counter)
+                    tf.summary.scalar("disc - accuracy", disc_train_accuracy.result() * 100, step=train_counter)
+
+        # Reset the metrics for the next epoch
+        gen_train_loss.reset_states()
+        disc_train_loss.reset_states()
+        gen_train_accuracy.reset_states()
+        disc_train_accuracy.reset_states()
+
+    generator.save_weights(gen_weights_path)
+    discriminator.save_weights(disc_weights_path)
+    gen_train_summary_writer.close()
+    gen_train_summary_writer.close()
+    t1.close()
+    t2.close()
 
 def Q1(epochs=10, save_img_every=100):
     generator = exModels.CNNGenerator()
@@ -162,11 +240,19 @@ def Q2(epochs=10, save_img_every=100, noise_attributes: Dict = None):
     train_AE(generator, train_ds, epochs, save_img_every, weights_path=DenoisingAE_WEIGHTS_PATH)
     visual_latent_space(generator, test_ds)
 
+def Q3(epochs=10, save_img_every=100):
+    generator = exModels.Generator()
+    discriminator = exModels.Discriminator()
+    test_ds, train_ds = get_data_as_tensorslice()
+    exModels.printable_model(generator).summary()
+    exModels.printable_model(discriminator).summary()
+    train_GAN(generator,discriminator , train_ds, epochs, save_img_every, gen_weights_path=GanGenerator_WEIGHTS_PATH, disc_weights_path=GanDiscriminator_WEIGHTS_PATH)
 
 def main():
-    visual_latent_space_from_save()
+    # visual_latent_space_from_save()
     # Q1(epochs=10, save_img_every=100)
     # Q2(epochs=10, save_img_every=100)
+    Q3(epochs=10, save_img_every=100)
 
 
 if __name__ == '__main__':
