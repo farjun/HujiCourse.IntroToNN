9c337741d6cc13e0e553184c6f13ce6a4f9f5344
omer <omerbh1@gmail.com>
2020-01-22 00:33:58 +0200

    expirementing

--- models.py
+++ models.py
@@ -11,45 +11,59 @@ def printable_model(model: Model, input_shape=(28, 28, 1)):
 
 class CNNGenerator(Model):
 
-    def __init__(self, lastActivation = 'sigmoid', activation = LeakyReLU()):
+    def __init__(self, lastActivation = 'sigmoid'):
         super(CNNGenerator, self).__init__()
-        self.conv1 = Conv2D(32, (3, 3), strides=(2, 2), padding='same', activation=activation)
         self.batchNormalization1 = BatchNormalization()
-        self.conv2 = Conv2D(64, (3, 3), strides=(2, 2), padding='same', activation=activation)
         self.batchNormalization2 = BatchNormalization()
-        self.flatten = Flatten()
-        self.d1 = Dense(512, activation=activation)
         self.batchNormalization3 = BatchNormalization()
-        self.d2 = Dense(10, activation=activation)
         self.batchNormalization4 = BatchNormalization()
-        self.d3 = Dense(512, activation=activation)
         self.batchNormalization5 = BatchNormalization()
-        self.d4 = Dense(7 * 7 * 64, activation=activation)
         self.batchNormalization6 = BatchNormalization()
 
+        self.activation1 = LeakyReLU()
+        self.activation2 = LeakyReLU()
+        self.activation3 = LeakyReLU()
+        self.activation4 = LeakyReLU()
+        self.activation5 = LeakyReLU()
+        self.activation6 = LeakyReLU()
+
+        self.conv1 = Conv2D(32, (3, 3), strides=(2, 2), padding='same')
+        self.conv2 = Conv2D(64, (3, 3), strides=(2, 2), padding='same')
+        self.flatten = Flatten()
+        self.d1 = Dense(512)
+        self.d2 = Dense(10)
+        self.d3 = Dense(512)
+        self.d4 = Dense(7 * 7 * 64)
+
         self.reshaper = Reshape((7, 7, 64))
-        self.conv3 = Conv2DTranspose(64, (3, 3), strides=(2, 2), padding='same', activation=activation)
+        self.conv3 = Conv2DTranspose(64, (3, 3), strides=(2, 2), padding='same')
         self.conv4 = Conv2DTranspose(1, (3, 3), strides=(2, 2), padding='same', activation=lastActivation)
 
     def encode(self, x, **kwargs):
         x = self.conv1(x)
         x = self.batchNormalization1(x)
+        x = self.activation1(x)
         x = self.conv2(x)
         x = self.batchNormalization2(x)
+        x = self.activation2(x)
         x = self.flatten(x)
         x = self.d1(x)
         x = self.batchNormalization3(x)
+        x = self.activation3(x)
         x = self.d2(x)
         return x
 
     def decode(self, x, **kwargs):
         x = self.d3(x)
         x = self.batchNormalization4(x)
+        x = self.activation4(x)
         x = self.d4(x)
         x = self.batchNormalization5(x)
+        x = self.activation5(x)
         x = self.reshaper(x)
         x = self.conv3(x)
         x = self.batchNormalization6(x)
+        x = self.activation6(x)
         x = self.conv4(x)
         return x
 
